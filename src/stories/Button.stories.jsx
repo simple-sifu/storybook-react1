ad9cfa3b7e21243f415b46cda328d20e801fff0d
Tommy Han <tommy.han@ownbackup.com>
2023-08-25 14:05:29 -0400

    add red button

---
+++ src/stories/Button.stories.jsx
@@ -0,0 +1,9 @@
+import Button from '../components/Button'
+
+export default {
+    title: "Button",
+    component: Button
+}
+
+export const Red = () => <Button label="Press me" backgroundColor="red" />
+
